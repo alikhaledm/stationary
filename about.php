0733f74a12db9eda3e480e3a21d222e3b641bd6d
mariamaged30 <132774322+mariamaged30@users.noreply.github.com>
2023-05-15 18:01:49 +0300

    aboutus

---
+++ about.php
@@ -0,0 +1,64 @@
+<?php
+include("navbar.php")
+?>
+<html>
+<title>School Supplies List</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<link rel="stylesheet" href="styles.css">
+<head>
+<style>
+    h1 {
+      font-family: "Avenir Light", sans-serif;
+      font-size: 60px;
+      text-align: center;
+    }
+    
+    h2 {
+      text-align: center;
+      font-size: 55px;
+    }
+    
+    .center-align {
+      text-align: center;
+      font-size:130%;
+    }
+    
+    .paragraph-container {
+      display: flex;
+      justify-content: center;
+    }
+    
+    p {
+      max-width: 600px;
+     
+    }
+    
+    
+  </style>
+</head>
+<body>
+  <h1>About Us</h1>
+  
+  <div class="center-align paragraph-container">
+    <p>
+    At Supply Room, we are passionate about education and believe that every child deserves access to the tools they need to succeed in school. That's why we collaborate with schools and educational institutions to support underprivileged students through donation programs and initiatives
+    </p>
+  </div>
+  
+  <div style="text-align: center;">
+    <img src="images/aboutus.jpg" alt="About Us" style="max-width: 50%;">
+  </div>
+  
+  <h2>Our Story</h2>
+  
+  <div class="center-align paragraph-container">
+    <p>
+    Our story began with a shared passion for creating innovative solutions. We embarked on this journey with a vision to make a positive impact in the world. Through perseverance, dedication, and a strong belief in our mission, Today, our goal is to support parents by providing a convenient and hassle-free way to purchase school supplies. We understand the challenges parents face when it comes to preparing their children for school. We believe in simplifying the back-to-school shopping experience, saving parents valuable time and effort. With our user-friendly online platform and reliable delivery service, parents can shop with confidence, knowing that they are getting top-notch products for their children's education. We are committed to making the process seamless, so parents can focus on what matters most  supporting their children's learning journey
+    </p>
+  </div> <br> <br>
+  </body>
+<?php
+include("footer.php")
+?>
+
+</html> 
