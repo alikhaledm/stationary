08b6bd4f5f9b6697757ce8e7d4e71481f501f4eb
alikhaledm <100496591+alikhaledm@users.noreply.github.com>
2023-05-19 19:43:15 +0300

    backend for cart finished

--- cart.php
+++ cart.php
@@ -17,20 +17,14 @@ if ($result) {
     $total = $tagID['total_price'];
 }
 
-// Update quantity if the checker is checked
 if (isset($_POST['update_quantity'])) {
     $productId = $_POST['product_id'];
     $newQuantity = $_POST['quantity'];
 
     $updateQuery = "UPDATE cart SET quantity = $newQuantity WHERE userid = $userid AND productid = $productId";
     $updateResult = mysqli_query($conn, $updateQuery);
-
-    if ($updateResult) {
-        echo "Quantity updated successfully.";
-    } else {
-        echo "Error updating quantity: " . mysqli_error($conn);
-    }
 }
+
 ?>
 
 <html>
@@ -41,7 +35,7 @@ if (isset($_POST['update_quantity'])) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="cartstyles.css">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
@@ -64,31 +58,38 @@ if (isset($_POST['update_quantity'])) {
         }
 
         // Event listener for the checkbox
-        const checker = document.getElementById('checker');
-        const quantityInput = document.getElementById('quantity');
+        const updateButtons = document.getElementsByClassName('update-button');
+        const quantityInputs = document.getElementsByClassName('quantity');
 
-        checker.addEventListener('change', function() {
-            const productId = quantityInput.dataset.productId;
-            const newQuantity = this.checked ? quantityInput.value : 1;
-            updateQuantity(productId, newQuantity);
-        });
+        for (let i = 0; i < checkers.length; i++) {
+            const checker = checkers[i];
+            const quantityInput = quantityInputs[i];
+
+            updateButton.addEventListener('click', function(e) {
+                const productId = quantityInput.dataset.productId;
+                const newQuantity = this.checked ? quantityInput.value : 1;
+                updateQuantity(productId, newQuantity);
+            });
+        }
     </script>
+
 </head>
 
 <body>
     <div class="card">
         <div class="row">
             <div class="col-md-8 cart">
-                <div class="title">
+                <div class="title" style="margin-bottom: 20; padding-bottom:0%;">
                     <div class="row">
                         <div class="col">
-                            <h4><b>Shopping Cart</b></h4>
+                            <h4 style="margin-bottom: 0; padding-bottom:0%;"><b>Shopping Cart</b></h4>
                         </div>
                         <div class="col align-self-center text-right text-muted"> 3 items</div>
                     </div>
                 </div>
 
                 <?php
+
                 $getProducts = "SELECT * FROM cart WHERE userid = $userid";
                 $result2 = mysqli_query($conn, $getProducts);
                 if ($result2) {
@@ -102,29 +103,29 @@ if (isset($_POST['update_quantity'])) {
                                 $quantity = $rowData['quantity'];
                                 $productPrice = $rowData2['price'];
 
+                                // Truncate product name if more than 15 characters
+                                $productName = strlen($rowData2['pname']) > 25 ? substr($rowData2['pname'], 0, 25) . ".." : $rowData2['pname'];
+
                                 echo '
-                <div class="row border-top border-bottom">
-                    <div class="row main align-items-center" style="padding:0;">
-                        <div class="col-2"><img class="img-fluid" src="images/Shop/products/' . $rowData2['photo'] . '"></div>
-                        <div class="col">
-                            <div class="row text-muted">' . $rowData2['category'] . '</div>
-                            <div class="row">' . $rowData2['pname'] . '</div>
+    <div class="row border-top border-bottom">
+        <div class="row main align-items-center" style="padding: 0;">
+            <div class="col-2"><img class="img-fluid" src="images/Shop/products/' . $rowData2['photo'] . '"></div>
+            <div class="col-4" >
+                <div class="row product-name"><b>' . $productName . '</b></div>
+            </div>
+                <div class="col-2">
+                    <form action="" method="POST" class="update-form" style="margin-bottom:0;">
+                        <input type="hidden" name="product_id" value="' . $productId . '">
+                        <div class="quantity-input">
+                            <input type="number" name="quantity" class="quantity" min="1" value="' . $quantity . '" data-product-id="' . $productId . '">
+                            <button type="submit" name="update_quantity" class="update-button"><i class="fa fa-check checker" style="color:black; width:0"></i></button>
                         </div>
-                        <div class="row">
-                            <div class="col">
-                                <form action="" method="POST" class="update-form">
-                                    <input type="hidden" name="product_id" value="' . $productId . '">
-                                    <div class="quantity-input">
-                                        <input type="number" name="quantity" id="quantity" min="1" value="' . $quantity . '" data-product-id="' . $productId . '">
-                                        <button type="submit" name="update_quantity" class="update-button"><i class="fa fa-check-circle" id="checker" style="color:green;"></i></button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="col">$' . ($productPrice * $quantity) . '</div>
-                        <div class="col"><a href="cart.php?remove=' . $cartId . '"><span class="close">&#10005;</span></a></div>
-                    </div>
-                </div>';
+                    </form>
+            </div>
+            <div class="col-3" style="text-align:right; padding-right:0;">$' . number_format($productPrice, 2) . ' <b>x' . $quantity . '</b></div>
+            <div class="col-1"><a href="removeCart.php?varname=' . $productId . '"><span>&#10005;</span></a></div>
+        </div>
+    </div>';
                             }
                         }
                     }
@@ -138,18 +139,30 @@ if (isset($_POST['update_quantity'])) {
                     <h5><b>Summary</b></h5>
                 </div>
                 <hr>
-                <form>
-                    <p>SHIPPING</p>
-                    <select>
-                        <option class="text-muted">In Egypt Delivery - EGP50.00</option>
-                        <option class="text-muted">International Shipping - EGP350.00</option>
-                    </select>
-                    <p>GIVE CODE</p>
-                    <input id="code" placeholder="Enter your code">
-                </form>
-                <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
-                    <div class="col">TOTAL PRICE</div>
-                    <div class="col text-right">$<?php echo number_format($total, 2); ?></div>
+                <div class="row" style="margin: 0; padding:0%;">
+                    <div class="col" style="margin: 0; padding:0%;">
+                        <form id="update-form">
+                            <b>
+                                <p>Shipping</p>
+                            </b>
+                            <select name="shipping" class="shipping" style="margin: 0;" id="shipping">
+                                <option class="text-muted" value="1">In Egypt Delivery - EGP50.00</option>
+                                <option class="text-muted" value="2">International Shipping - EGP350.00</option>
+                            </select>
+                            <button type="submit" name="updatetotal" class="update-button"><i class="fa fa-check checker" style="color:black; width:0"></i></button>
+                        </form>
+                    </div>
+                    <div class="row" style="margin: 0; padding:0%;">
+                        <b>
+                            <p>Discount Code</p>
+                        </b>
+                        <input name="code" placeholder="Enter your code" class="shipping">
+                    </div>
+                </div>
+
+                <div class=" row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
+                    <div class="col">TOTAL PRICE (including shipping)</div>
+                    <div class="col" id="total-price" data-total="<?php echo $total; ?>">$<?php echo number_format($total, 2); ?></div>
                 </div>
                 <button class="btn" href="checkout.php">CHECKOUT</button>
             </div>
