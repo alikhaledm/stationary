6a4ab606a9ac3cf66bc05fb3cf34a89d7a6228bd
alikhaledm <alikhaledm399@gmail.com>
2023-06-04 03:56:52 +0200

    admin panel updates

--- admin-AddSuppliesList.php
+++ admin-AddSuppliesList.php
@@ -2,24 +2,49 @@
 <html lang="en">
 <?php
 require_once("connect.php");
-error_reporting(0);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $pname = $_POST['pname'];
-    $pdesc = $_POST['pdesc'];
-    $category = $_POST['category'];
-    $photo = $_POST['photo'];
+    $lname = $_POST['lname'];
+    $school = $_POST['school'];
+    $grade = $_POST['grade'];
     $price = $_POST['price'];
+    $products = $_POST['product'];
+    $categories = $_POST['category'];
+    $quantities = $_POST['quantity'];
 
-    $query = "INSERT INTO products (pname, pdesc, category, photo, price) VALUES ('$pname', '$pdesc', '$category', '$photo', '$price')";
+    $query = "INSERT INTO supplies_list (listname, school_id, grade, total_price) VALUES ('$lname', '$school', '$grade', '$price')";
     $result = mysqli_query($conn, $query);
+
     if ($result) {
-        echo "<script> alert('Product Added') </script>";
+        $listid = mysqli_insert_id($conn);
+
+        if (count($products) > 0) {
+            for ($i = 0; $i < count($products); $i++) {
+                $product = $products[$i];
+                $category = $categories[$i];
+                $quantity = $quantities[$i];
+
+                $query = "INSERT INTO supplylistitems (supplylistid, productid, prodcategory, quantity) VALUES ('$listid', '$product', '$category', '$quantity')";
+                $result = mysqli_query($conn, $query);
+
+                if (!$result) {
+                    echo "<script>alert('Failed to insert product data!');</script>";
+                    break;
+                }
+            }
+        }
+
+        if ($result) {
+            echo "<script>alert('Supply list added successfully!');</script>";
+        } else {
+            echo "<script>alert('Supply list failed to add!');</script>";
+        }
     } else {
-        echo "<script> alert('Product not Added') </script>";
+        echo "<script>alert('Supply list failed to add!');</script>";
     }
 }
 ?>
+
 
 <head>
     <meta charset="utf-8">
@@ -96,7 +121,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                         ?>
                                                     </select></td>
                                                 <td> <input name='price' type='text' class="form-control" required></td>
-                                                <td><input type="file" name="upload" accept="application/pdf,application/vnd.ms-excel" class="form-control" required></td>
+                                                <td><input type="file" name="upload" accept="application/pdf,application/vnd.ms-excel" class="form-control"></td>
                                             </tr>
                                         </tbody>
                                         <thead>
@@ -104,13 +129,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                 <th>Product</th>
                                                 <th>Category</th>
                                                 <th>Quantity</th>
-
+                                                <th>
+                                                    <button type="button" class="btn btn-none py-0" style="height: 1; color:green;" onclick="duplicateRow()"><svg xmlns="http://www.w3.org/2000/svg" width="22" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                                                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
+                                                        </svg></button>
+                                                </th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
+                                            <tr id="row1">
                                                 <td>
-                                                    <select name="product" class="form-select">
+                                                    <select name="product[]" class="form-select">
                                                         <option value="">No product selected</option>
                                                         <?php
                                                         $query = "SELECT * FROM products";
@@ -124,7 +153,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                         ?>
                                                     </select>
                                                 </td>
-                                                <td><select name="category" class="form-select" required>
+                                                <td><select name="category[]" class="form-select" required>
                                                         <option value="">No category selected</option>
                                                         <option value="Health & Hygiene Kit">Health & Hygiene Kit</option>
                                                         <option value="Academic Kit">Academic Kit</option>
@@ -141,7 +170,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                         <option value="Music">Music</option>
                                                         <option value="PE">PE</option>
                                                     </select></td>
-                                                <td><input type='text' name='namechild' class="form-control" required></td>
+                                                <td><input type='text' name='quantity[]' class="form-control" required></td>
+                                            </tr>
+                                            <tr>
                                             </tr>
                                         </tbody>
                                     </table>
@@ -158,6 +189,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <script src="assets/js/bs-init.js"></script>
                 <script src="assets/js/theme.js"></script>
                 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                <script>
+                    function duplicateRow() {
+                        var lastRow = document.getElementById("row1");
+                        var clonedRow = lastRow.cloneNode(true);
+                        lastRow.parentNode.insertBefore(clonedRow, lastRow.nextSibling);
+                    }
+                </script>
+
+
 </body>
 
 </html>
